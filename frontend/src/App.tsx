1cfd1f8216cfa33860489c6d30a9bee4074d4bb5
JongBeom Lee <jongbeom.lee@navercorp.com>
2024-01-10 17:50:22 +0900

    Fix formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./App.css";
 import { Button } from "@mui/material";
 
