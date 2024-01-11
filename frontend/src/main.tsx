80468c1a34ad047b38506dc158f72283c79b7968
JongBeom Lee <jongbeom.lee@navercorp.com>
2024-01-11 15:32:22 +0900

    Add editor header

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import EditorLayout from "./components/layouts/EditorLayot";
+import EditorIndex from "./pages/editor/Index";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <EditorLayout />,
+		children: [
+			{
+				path: ":documentId",
+				element: <EditorIndex />,
+			},
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
---
+++ frontend/src/pages/editor/Index.tsx
@@ -0,0 +1,5 @@
+function EditorIndex() {
+	return <div>Hi</div>;
+}
+
+export default EditorIndex;
