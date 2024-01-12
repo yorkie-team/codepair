61633e5375b02b2ffdf215ac935bdc6eeee707be
devleejb <devleejb@gmail.com>
2024-01-12 11:33:26 +0900

    Add env file

--- frontend/src/pages/editor/Index.tsx
+++ frontend/src/pages/editor/Index.tsx
@@ -3,21 +3,33 @@ import Editor from "../../components/editor/Editor";
 import * as yorkie from "yorkie-js-sdk";
 import { setClient, setDoc } from "../../store/editorSlice";
 import { useDispatch } from "react-redux";
+import {
+	YorkieCodeMirrorDocType,
+	YorkieCodeMirrorPresenceType,
+} from "../../utils/yorkie/yorkieSync";
+import randomColor from "randomcolor";
+import Color from "color";
 
 function EditorIndex() {
 	const dispatch = useDispatch();
 
+	console.log(import.meta.env);
 	useEffect(() => {
 		const initializeYorkie = async () => {
-			const client = new yorkie.Client("https://api.yorkie.dev", {
-				apiKey: "",
+			const client = new yorkie.Client(import.meta.env.VITE_YORKIE_API_ADDR, {
+				apiKey: import.meta.env.VITE_YORKIE_API_KEY,
 			});
 			await client.activate();
 
-			const doc = new yorkie.Document("my-first-document");
+			const doc = new yorkie.Document<YorkieCodeMirrorDocType, YorkieCodeMirrorPresenceType>(
+				"my-first-document"
+			);
 
+			console.log(Color(randomColor()).fade(0.15).toString());
 			await client.attach(doc, {
 				initialPresence: {
+					name: "Yorkie",
+					color: Color(randomColor()).fade(0.15).toString(),
 					selection: null,
 				},
 			});
