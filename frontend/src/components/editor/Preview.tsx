968a7489d8ec61a5623aa073718fbc1871fc27d3
devleejb <devleejb@gmail.com>
2024-01-12 18:09:29 +0900

    Add document creation logic

--- frontend/src/components/editor/Preview.tsx
+++ frontend/src/components/editor/Preview.tsx
@@ -5,16 +5,18 @@ import { selectEditor } from "../../store/editorSlice";
 import { CircularProgress } from "@mui/material";
 import { useEffect, useState } from "react";
 import "./editor.css";
+import { useParams } from "react-router-dom";
 
 function Preview() {
+	const params = useParams();
 	const currentTheme = useCurrentTheme();
 	const editorStore = useSelector(selectEditor);
 	const [content, setContent] = useState("");
 
 	useEffect(() => {
-		if (!editorStore.doc) return;
+		if (!editorStore.doc || !params.documentId) return;
 
-		setContent(editorStore.doc?.getRoot().content.toString());
+		setContent(editorStore.doc?.getRoot().content?.toString() || "");
 
 		const unsubsribe = editorStore.doc.subscribe("$.content", () => {
 			setContent(editorStore.doc?.getRoot().content.toString() as string);
@@ -22,8 +24,9 @@ function Preview() {
 
 		return () => {
 			unsubsribe();
+			setContent("");
 		};
-	}, [editorStore.doc]);
+	}, [editorStore.doc, params.documentId]);
 
 	if (!editorStore?.doc) return <CircularProgress sx={{ marginX: "auto", mt: 4 }} />;
 
