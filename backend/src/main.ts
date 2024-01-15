4d45d43caf9addd8bd23f4231a00c76bb7de4713
LeeJongBeom <52884648+devleejb@users.noreply.github.com>
2024-01-15 14:24:17 +0900

    Backend configuration (#44)
    * Initialize NestJS project
    
    * Initialize `Prisma` orm
    
    * Add lint options
    
    * Add swagger
    
    * Change format options in frontend & backend
    
    * Add CI for backend

---
+++ backend/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle("CodePair")
+		.setDescription("The CodePair API description")
+		.setVersion("1.0")
+		.addTag("codepair")
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api", app, document);
+
+	await app.listen(3000);
+}
+bootstrap();
