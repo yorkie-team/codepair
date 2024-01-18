ed1c2659e1cdc70f1031fb1efb69e03a5f2203cd
devleejb <devleejb@gmail.com>
2024-01-18 15:05:46 +0900

    Add service for retrieving document list in the workspace

---
+++ backend/src/workspace-documents/types/find-workspace-documents-response.type.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { WorkspaceDocumentDomain } from "./workspace-document-domain.type";
+
+export class FindWorkspaceUsersResponse {
+	@ApiProperty({
+		type: [WorkspaceDocumentDomain],
+		description: "List of found workspace documents",
+	})
+	documents: Array<WorkspaceDocumentDomain>;
+
+	@ApiProperty({ type: String, description: "The ID of last document" })
+	cursor: string | null;
+}
--- backend/src/workspace-documents/workspace-documents.service.ts
+++ backend/src/workspace-documents/workspace-documents.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
+import { Prisma } from "@prisma/client";
 import { PrismaService } from "src/db/prisma.service";
+import { FindWorkspaceUsersResponse } from "./types/find-workspace-documents-response.type";
 
 @Injectable()
 export class WorkspaceDocumentsService {
@@ -43,5 +45,45 @@ export class WorkspaceDocumentsService {
 		} catch (e) {
 			throw new NotFoundException();
 		}
+	}
+
+	async findMany(
+		userId: string,
+		workspaceId: string,
+		pageSize: number,
+		cursor?: string
+	): Promise<FindWorkspaceUsersResponse> {
+		try {
+			await this.prismaService.userWorkspace.findFirstOrThrow({
+				where: {
+					userId,
+					workspaceId,
+				},
+			});
+		} catch (e) {
+			throw new NotFoundException();
+		}
+
+		const additionalOptions: Prisma.DocumentFindManyArgs = {};
+
+		if (cursor) {
+			additionalOptions.cursor = { id: cursor };
+		}
+
+		const documentList = await this.prismaService.document.findMany({
+			take: pageSize + 1,
+			where: {
+				workspaceId,
+			},
+			orderBy: {
+				id: "desc",
+			},
+			...additionalOptions,
+		});
+
+		return {
+			documents: documentList.slice(0, pageSize),
+			cursor: documentList.length > pageSize ? documentList[pageSize].id : null,
+		};
 	}
 }
