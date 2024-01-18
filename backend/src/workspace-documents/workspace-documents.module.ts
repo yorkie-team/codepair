69a84e1e9515c8fbc0c165633aacf54df2830977
devleejb <devleejb@gmail.com>
2024-01-18 17:07:29 +0900

    Add JwtModule to workspace-documents module for sharing

--- backend/src/workspace-documents/workspace-documents.module.ts
+++ backend/src/workspace-documents/workspace-documents.module.ts
@@ -2,8 +2,20 @@ import { Module } from "@nestjs/common";
 import { WorkspaceDocumentsService } from "./workspace-documents.service";
 import { WorkspaceDocumentsController } from "./workspace-documents.controller";
 import { PrismaService } from "src/db/prisma.service";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
+	imports: [
+		JwtModule.registerAsync({
+			useFactory: async (configService: ConfigService) => {
+				return {
+					secret: configService.get<string>("JWT_AUTH_SECRET"),
+				};
+			},
+			inject: [ConfigService],
+		}),
+	],
 	providers: [WorkspaceDocumentsService, PrismaService],
 	controllers: [WorkspaceDocumentsController],
 })
