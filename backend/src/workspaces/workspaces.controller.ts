f24ffdbb6682fc3c493c141ac9ead6d9e7792c15
LeeJongBeom <52884648+devleejb@users.noreply.github.com>
2024-01-18 11:26:16 +0900

    (BE) Add API for Retrieving a Workspace (#58)
    * Add service for retrieving a workspace
    
    * Add controller for retrieving a workspace

--- backend/src/workspaces/workspaces.controller.ts
+++ backend/src/workspaces/workspaces.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post, Req } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Req } from "@nestjs/common";
 import { WorkspacesService } from "./workspaces.service";
 import { CreateWorkspaceDto } from "./dto/CreateWorkspace.dto";
-import { ApiBearerAuth, ApiBody, ApiCreatedResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import {
+	ApiBearerAuth,
+	ApiBody,
+	ApiCreatedResponse,
+	ApiFoundResponse,
+	ApiNotFoundResponse,
+	ApiOperation,
+	ApiTags,
+} from "@nestjs/swagger";
 import { AuthroizedRequest } from "src/utils/types/req.type";
 import { CreateWorkspaceResponse } from "./types/create-workspace-response.type";
+import { FindWorkspaceResponse } from "./types/find-workspace-response.type";
+import { HttpExceptionResponse } from "src/utils/types/http-exception-response.type";
 
 @ApiTags("Workspaces")
 @ApiBearerAuth()
@@ -23,5 +33,19 @@ export class WorkspacesController {
 		@Body() createWorkspaceDto: CreateWorkspaceDto
 	): Promise<CreateWorkspaceResponse> {
 		return this.workspacesService.create(req.user.id, createWorkspaceDto.title);
+	}
+
+	@Get(":id")
+	@ApiOperation({
+		summary: "Retrieve a Workspace",
+		description: "If the user has the access permissions, return a workspace.",
+	})
+	@ApiFoundResponse({ type: FindWorkspaceResponse })
+	@ApiNotFoundResponse({
+		type: HttpExceptionResponse,
+		description: "The Workspace does not exist, or the user lacks the appropriate permissions.",
+	})
+	async findOne(@Req() req: AuthroizedRequest, @Param("id") workspaceId: string) {
+		return this.workspacesService.findOne(req.user.id, workspaceId);
 	}
 }
