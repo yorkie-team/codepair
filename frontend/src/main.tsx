5696e972548a37c5fb096af9fb1c05f52e8944fa
devleejb <devleejb@gmail.com>
2024-01-19 13:34:00 +0900

    Add react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,14 +6,18 @@ import { store } from "./store/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const persistor = persistStore(store);
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
-				<App />
+				<QueryClientProvider client={queryClient}>
+					<App />
+				</QueryClientProvider>
 			</PersistGate>
 		</Provider>
 	</React.StrictMode>
