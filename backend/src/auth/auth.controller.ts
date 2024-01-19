d3d9ffec780395faa4812b6bff0966300b9b7df7
devleejb <devleejb@gmail.com>
2024-01-19 13:01:37 +0900

    Add redirection to auth callback router

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import { Controller, Get, HttpRedirectResponse, Redirect, Req, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { LoginRequest } from "./types/login-request.type";
 import { JwtService } from "@nestjs/jwt";
@@ -6,11 +6,13 @@ import { LoginResponse } from "./types/login-response.type";
 import { UsersService } from "src/users/users.service";
 import { Public } from "src/utils/decorators/auth.decorator";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { ConfigService } from "@nestjs/config";
 
 @ApiTags("Auth")
 @Controller("auth")
 export class AuthController {
 	constructor(
+		private configService: ConfigService,
 		private jwtService: JwtService,
 		private usersService: UsersService
 	) {}
@@ -18,13 +20,14 @@ export class AuthController {
 	@Public()
 	@Get("login/github")
 	@Get("callback/github")
+	@Redirect()
 	@UseGuards(AuthGuard("github"))
 	@ApiOperation({
 		summary: "SignUp/LogIn with GitHub",
 		description: "SignUp/Login with GitHub social login",
 	})
 	@ApiResponse({ type: LoginResponse })
-	async login(@Req() req: LoginRequest): Promise<LoginResponse> {
+	async login(@Req() req: LoginRequest): Promise<HttpRedirectResponse> {
 		const user = await this.usersService.findOrCreate(
 			req.user.socialProvider,
 			req.user.socialUid,
@@ -33,6 +36,9 @@ export class AuthController {
 
 		const accessToken = this.jwtService.sign({ sub: user.id, nickname: user.nickname });
 
-		return { accessToken };
+		return {
+			url: `${this.configService.get("FRONTEND_BASE_URL")}/auth/callback?token=${accessToken}`,
+			statusCode: 302,
+		};
 	}
 }
