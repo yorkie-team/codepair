029bd1d8d924da7f6c625398b0784415ddd21afc
LeeJongBeom <52884648+devleejb@users.noreply.github.com>
2024-01-19 16:46:28 +0900

    Add logout logic (#71)

--- frontend/src/components/drawers/WorkspaceDrawer.tsx
+++ frontend/src/components/drawers/WorkspaceDrawer.tsx
@@ -6,15 +6,28 @@ import {
 	ListItem,
 	ListItemAvatar,
 	ListItemButton,
+	ListItemSecondaryAction,
 	ListItemText,
 } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../store/userSlice";
+import { MouseEventHandler, useState } from "react";
+import ProfilePopover from "../common/ProfilePopover";
 
 const DRAWER_WIDTH = 240;
 
 function WorkspaceDrawer() {
 	const userStore = useSelector(selectUser);
+	const [profileAnchorEl, setProfileAnchorEl] = useState<(EventTarget & Element) | null>(null);
+
+	const handleOpenProfilePopover: MouseEventHandler = (event) => {
+		setProfileAnchorEl(event.currentTarget);
+	};
+
+	const handleCloseProfilePopover = () => {
+		setProfileAnchorEl(null);
+	};
 
 	return (
 		<Drawer
@@ -33,12 +46,20 @@ function WorkspaceDrawer() {
 			<Box sx={{ mt: "auto" }}>
 				<Divider />
 				<ListItem disablePadding>
-					<ListItemButton>
+					<ListItemButton onClick={handleOpenProfilePopover}>
 						<ListItemAvatar>
 							<Avatar>{userStore.data?.nickname.charAt(0)}</Avatar>
 						</ListItemAvatar>
 						<ListItemText primary={userStore.data?.nickname} />
+						<ListItemSecondaryAction>
+							<MoreVertIcon />
+						</ListItemSecondaryAction>
 					</ListItemButton>
+					<ProfilePopover
+						open={Boolean(profileAnchorEl)}
+						anchorEl={profileAnchorEl}
+						onClose={handleCloseProfilePopover}
+					/>
 				</ListItem>
 			</Box>
 		</Drawer>
