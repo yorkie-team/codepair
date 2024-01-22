847929099d3bbfa10aa10493165e0ac230309e44
devleejb <devleejb@gmail.com>
2024-01-22 14:49:42 +0900

    Change parameter

--- frontend/src/pages/workspace/Index.tsx
+++ frontend/src/pages/workspace/Index.tsx
@@ -2,14 +2,20 @@ import { useParams } from "react-router-dom";
 import WorkspaceDrawer from "../../components/drawers/WorkspaceDrawer";
 import { useGetWorkspaceDocumentListQuery } from "../../hooks/api/workspaceDocument";
 import { useGetWorkspaceQuery } from "../../hooks/api/workspace";
+import { Box, Stack } from "@mui/material";
 
 function WorkspaceIndex() {
 	const params = useParams();
-	const { data: workspace } = useGetWorkspaceQuery(params.workspaceId);
+	const { data: workspace } = useGetWorkspaceQuery(params.workspaceSlug);
 	const { data } = useGetWorkspaceDocumentListQuery(workspace?.id);
 
-	console.log(data);
-	return <WorkspaceDrawer />;
+	console.log(workspace, data);
+	return (
+		<Stack direction="row">
+			<WorkspaceDrawer />
+			<Box>123</Box>
+		</Stack>
+	);
 }
 
 export default WorkspaceIndex;
