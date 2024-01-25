53f5d218d45358fe78d596d0426380337d63d874
devleejb <devleejb@gmail.com>
2024-01-25 13:44:37 +0900

    Add axios to backend project

--- backend/src/workspace-documents/workspace-documents.module.ts
+++ backend/src/workspace-documents/workspace-documents.module.ts
@@ -2,9 +2,22 @@ import { Module } from "@nestjs/common";
 import { WorkspaceDocumentsService } from "./workspace-documents.service";
 import { WorkspaceDocumentsController } from "./workspace-documents.controller";
 import { PrismaService } from "src/db/prisma.service";
+import { HttpModule } from "@nestjs/axios";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
-	imports: [],
+	imports: [
+		HttpModule.registerAsync({
+			useFactory: async (configService: ConfigService) => {
+				return {
+					headers: {
+						Authorization: configService.get<string>("YORKIE_PROJECT_SECRET_KEY"),
+					},
+				};
+			},
+			inject: [ConfigService],
+		}),
+	],
 	providers: [WorkspaceDocumentsService, PrismaService],
 	controllers: [WorkspaceDocumentsController],
 })
