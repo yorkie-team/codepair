c94d8ee50c0ce9ded576b1bfed0d84ce31e5f3e9
devleejb <devleejb@gmail.com>
2024-01-29 17:32:43 +0900

    Add intelligence feature controller

--- backend/src/intelligence/intelligence.controller.ts
+++ backend/src/intelligence/intelligence.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Req } from "@nestjs/common";
+import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiParam, ApiTags } from "@nestjs/swagger";
+import { IntelligenceService } from "./intelligence.service";
+import { AuthroizedRequest } from "src/utils/types/req.type";
 
+@ApiTags("Intelligences")
+@ApiBearerAuth()
 @Controller("intelligence")
-export class IntelligenceController {}
+export class IntelligenceController {
+	constructor(private intelligenceService: IntelligenceService) {}
+
+	@Get(":feature")
+	@ApiOperation({
+		summary: "Run the Yorkie Intelligence Feature",
+		description: "Run the selected yorkie intelligence",
+	})
+	@ApiParam({
+		name: "feature",
+		description: "Feature of intelligence to run",
+	})
+	@ApiOkResponse({ type: String })
+	async runFeature(@Req() req: AuthroizedRequest): Promise<string> {
+		return "Test";
+	}
+}
--- backend/src/intelligence/intelligence.service.ts
+++ backend/src/intelligence/intelligence.service.ts
@@ -3,5 +3,5 @@ import { BaseChatModel } from "@langchain/core/language_models/chat_models";
 
 @Injectable()
 export class IntelligenceService {
-	constructor(@Inject("BaseChatModel") private chatModel: BaseChatModel) {}
+	constructor(@Inject("ChatModel") private chatModel: BaseChatModel) {}
 }
