05e47cdb03126a3a2ae52a2e7b4e63d7b97067ef
devleejb <devleejb@gmail.com>
2024-02-27 20:34:58 +0900

    Add KaTex support

--- frontend/src/components/editor/Preview.tsx
+++ frontend/src/components/editor/Preview.tsx
@@ -6,6 +6,11 @@ import { CircularProgress, Stack } from "@mui/material";
 import { useEffect, useState } from "react";
 import "./editor.css";
 import { addSoftLineBreak } from "../../utils/document";
+import katex from "katex";
+import { getCodeString } from "rehype-rewrite";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import "katex/dist/katex.min.css";
 
 function Preview() {
 	const currentTheme = useCurrentTheme();
@@ -48,6 +53,44 @@ function Preview() {
 				style: {
 					whiteSpace: "wrap !important",
 					WebkitUserModify: "read-only",
+				},
+			}}
+			remarkPlugins={[remarkMath]}
+			rehypePlugins={[rehypeKatex]}
+			components={{
+				code: ({ children = [], className, ...props }) => {
+					// https://www.npmjs.com/package/@uiw/react-markdown-preview#support-custom-katex-preview
+					if (typeof children === "string" && /^\$\$(.*)\$\$/.test(children)) {
+						const html = katex.renderToString(children.replace(/^\$\$(.*)\$\$/, "$1"), {
+							throwOnError: false,
+						});
+						return (
+							<code
+								dangerouslySetInnerHTML={{ __html: html }}
+								style={{ background: "transparent" }}
+							/>
+						);
+					}
+					const code =
+						props.node && props.node.children
+							? getCodeString(props.node.children)
+							: children;
+					if (
+						typeof code === "string" &&
+						typeof className === "string" &&
+						/^language-katex/.test(className.toLocaleLowerCase())
+					) {
+						const html = katex.renderToString(code, {
+							throwOnError: false,
+						});
+						return (
+							<code
+								style={{ fontSize: "150%" }}
+								dangerouslySetInnerHTML={{ __html: html }}
+							/>
+						);
+					}
+					return <code className={String(className)}>{children}</code>;
 				},
 			}}
 		/>
