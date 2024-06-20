121ee8b2e559e1fd814543ce1a015aeba725ae35
devleejb <devleejb@gmail.com>
2024-06-20 22:52:42 +0900

    Implement Workspace Page

--- frontend/src/components/drawers/WorkspaceDrawer.tsx
+++ frontend/src/components/drawers/WorkspaceDrawer.tsx
@@ -1,5 +1,4 @@
 import {
-	Button,
 	Divider,
 	Drawer,
 	IconButton,
@@ -11,13 +10,9 @@ import {
 } from "@mui/material";
 import { useSelector } from "react-redux";
 import { MouseEventHandler, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import WorkspaceListPopover from "../popovers/WorkspaceListPopover";
-import AddIcon from "@mui/icons-material/Add";
-import CreateModal from "../modals/CreateModal";
-import { useCreateDocumentMutation } from "../../hooks/api/workspaceDocument";
 import PeopleIcon from "@mui/icons-material/People";
 import MemberModal from "../modals/MemberModal";
 import { selectWorkspace } from "../../store/workspaceSlice";
@@ -29,15 +24,10 @@ interface WorkspaceDrawerProps {
 
 function WorkspaceDrawer(props: WorkspaceDrawerProps) {
 	const { open } = props;
-	const navigate = useNavigate();
 	const workspaceStore = useSelector(selectWorkspace);
-	const { mutateAsync: createDocument } = useCreateDocumentMutation(
-		workspaceStore.data?.id || ""
-	);
 	const [workspaceListAnchorEl, setWorkspaceListAnchorEl] = useState<
 		(EventTarget & Element) | null
 	>(null);
-	const [createWorkspaceModalOpen, setCreateWorkspaceModalOpen] = useState(false);
 	const [memberModalOpen, setMemberModalOpen] = useState(false);
 
 	const handleOpenWorkspacePopover: MouseEventHandler = (event) => {
@@ -46,16 +36,6 @@ function WorkspaceDrawer(props: WorkspaceDrawerProps) {
 
 	const handleCloseWorkspacePopover = () => {
 		setWorkspaceListAnchorEl(null);
-	};
-
-	const handleCreateWorkspace = async (data: { title: string }) => {
-		const document = await createDocument(data);
-
-		navigate(document.id);
-	};
-
-	const handleCreateWorkspaceModalOpen = () => {
-		setCreateWorkspaceModalOpen((prev) => !prev);
 	};
 
 	const handleMemberModalOpen = () => {
@@ -105,19 +85,6 @@ function WorkspaceDrawer(props: WorkspaceDrawerProps) {
 				</ListItem>
 			</WorkspaceDrawerHeader>
 			<Divider />
-			<ListItem>
-				<Button
-					variant="contained"
-					startIcon={<AddIcon />}
-					sx={{
-						width: 1,
-					}}
-					onClick={handleCreateWorkspaceModalOpen}
-				>
-					New Note
-				</Button>
-			</ListItem>
-			<Divider />
 			<ListItem disablePadding>
 				<ListItemButton onClick={handleMemberModalOpen}>
 					<ListItemIcon>
@@ -126,12 +93,6 @@ function WorkspaceDrawer(props: WorkspaceDrawerProps) {
 					<ListItemText primary="Members" />
 				</ListItemButton>
 			</ListItem>
-			<CreateModal
-				open={createWorkspaceModalOpen}
-				title="Note"
-				onSuccess={handleCreateWorkspace}
-				onClose={handleCreateWorkspaceModalOpen}
-			/>
 			<MemberModal open={memberModalOpen} onClose={handleMemberModalOpen} />
 		</Drawer>
 	);
--- frontend/src/pages/workspace/Index.tsx
+++ frontend/src/pages/workspace/Index.tsx
@@ -1,21 +1,28 @@
-import { useParams } from "react-router-dom";
-import WorkspaceDrawer from "../../components/drawers/WorkspaceDrawer";
-import { useGetWorkspaceDocumentListQuery } from "../../hooks/api/workspaceDocument";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+	useCreateDocumentMutation,
+	useGetWorkspaceDocumentListQuery,
+} from "../../hooks/api/workspaceDocument";
 import { useGetWorkspaceQuery } from "../../hooks/api/workspace";
-import { Box, CircularProgress, Grid, Stack } from "@mui/material";
+import { Box, Button, CircularProgress, Grid, Stack, Typography } from "@mui/material";
 import DocumentCard from "../../components/cards/DocumentCard";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Document } from "../../hooks/api/types/document.d";
 import InfiniteScroll from "react-infinite-scroller";
+import CreateModal from "../../components/modals/CreateModal";
+import AddIcon from "@mui/icons-material/Add";
 
 function WorkspaceIndex() {
 	const params = useParams();
+	const navigate = useNavigate();
 	const { data: workspace } = useGetWorkspaceQuery(params.workspaceSlug);
 	const {
 		data: documentPageList,
 		fetchNextPage,
 		hasNextPage,
 	} = useGetWorkspaceDocumentListQuery(workspace?.id);
+	const { mutateAsync: createDocument } = useCreateDocumentMutation(workspace?.id || "");
+	const [createDocumentModalOpen, setCreateDocumentModalOpen] = useState(false);
 	const documentList = useMemo(() => {
 		return (
 			documentPageList?.pages.reduce((prev, page) => {
@@ -24,42 +31,71 @@ function WorkspaceIndex() {
 		);
 	}, [documentPageList?.pages]);
 
+	const handleCreateDocumentModalOpen = () => {
+		setCreateDocumentModalOpen((prev) => !prev);
+	};
+
+	const handleCreateWorkspace = async (data: { title: string }) => {
+		const document = await createDocument(data);
+
+		navigate(document.id);
+	};
+
 	return (
-		<Stack direction="row">
-			{/* <WorkspaceDrawer />
-			<Box
-				style={{
-					maxHeight: "100vh",
-					overflow: "auto",
-				}}
-				width={1}
-			>
-				<InfiniteScroll
-					pageStart={0}
-					loadMore={() => fetchNextPage()}
-					hasMore={hasNextPage}
-					loader={
-						<Box className="loader" key={0}>
-							<CircularProgress size="sm" />
-						</Box>
-					}
-					useWindow={false}
+		<Stack
+			style={{
+				maxHeight: "100vh",
+				overflow: "auto",
+			}}
+			width={1}
+			gap={2}
+		>
+			<Stack direction="row" justifyContent="space-between" alignItems="center" px={2}>
+				<Typography variant="h5" fontWeight="bold">
+					{workspace?.title}{" "}
+					<Typography component="span" variant="inherit" color="primary">
+						{documentPageList?.pages[0].documents.length}
+					</Typography>
+				</Typography>
+				<Button
+					variant="contained"
+					startIcon={<AddIcon />}
+					onClick={handleCreateDocumentModalOpen}
 				>
-					<Box p={2} width={1}>
-						<Grid
-							container
-							spacing={{ xs: 2, md: 3 }}
-							columns={{ xs: 4, sm: 8, md: 12, lg: 12 }}
-						>
-							{documentList.map((document) => (
-								<Grid key={document.id} item xs={4} sm={4} md={4} lg={3}>
-									<DocumentCard document={document} />
-								</Grid>
-							))}
-						</Grid>
-					</Box>
-				</InfiniteScroll>
-			</Box> */}
+					New Note
+				</Button>
+			</Stack>
+			<InfiniteScroll
+				pageStart={0}
+				loadMore={() => fetchNextPage()}
+				hasMore={hasNextPage}
+				loader={
+					<Stack className="loader" key={0}>
+						<CircularProgress size="sm" />
+					</Stack>
+				}
+				useWindow={false}
+			>
+				<Box p={2} width={1}>
+					<Grid
+						container
+						spacing={{ xs: 2, md: 3 }}
+						columns={{ xs: 4, sm: 8, md: 12, lg: 12 }}
+					>
+						{documentList.map((document) => (
+							<Grid key={document.id} item xs={4} sm={4} md={4} lg={3}>
+								<DocumentCard document={document} />
+							</Grid>
+						))}
+					</Grid>
+				</Box>
+			</InfiniteScroll>
+			<CreateModal
+				open={createDocumentModalOpen}
+				title="Note"
+				onSuccess={handleCreateWorkspace}
+				onClose={handleCreateDocumentModalOpen}
+			/>
 		</Stack>
 	);
 }
