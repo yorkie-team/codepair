685f4c8a415fa497825c7bf045d5a9a7a6658b2f
devleejb <devleejb@gmail.com>
2024-06-27 00:28:15 +0900

    Setup sentry sourcemap upload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import version from "vite-plugin-package-version";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), version()],
+	build: {
+		sourcemap: true, // Source map generation must be turned on
+	},
+	plugins: [
+		react(),
+		version(),
+		// Only enable Sentry plugin if the environment variables are set
+		process.env.SENTRY_AUTH_TOKEN &&
+			sentryVitePlugin({
+				org: process.env.SENTRY_ORG,
+				project: process.env.SENTRY_PROJECT,
+				authToken: process.env.SENTRY_AUTH_TOKEN,
+			}),
+	],
 });
