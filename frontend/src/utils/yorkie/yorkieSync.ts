b26ed6920704336225dbc1265a5b65d5cbefb88c
LeeJongBeom <52884648+devleejb@users.noreply.github.com>
2024-06-28 02:25:40 +0900

    Add console.log for debugging document sync issue in CodePair (#221)
    * Add some console.log for debugging
    
    * Send error message to sentry

--- frontend/src/utils/yorkie/yorkieSync.ts
+++ frontend/src/utils/yorkie/yorkieSync.ts
@@ -1,6 +1,7 @@
 import * as cmState from "@codemirror/state";
 import * as cmView from "@codemirror/view";
 import * as yorkie from "yorkie-js-sdk";
+import * as Sentry from "@sentry/react";
 
 export type YorkieCodeMirrorDocType = {
 	content: yorkie.Text<yorkie.Indexable>;
@@ -105,6 +106,17 @@ class YorkieSyncPluginValue implements cmView.PluginValue {
 					});
 					adj += insertText.length - (toA - fromA);
 				});
+			}
+			const isSuccessful =
+				this.view.state.doc.toString() === this._doc.getRoot().content.toString();
+
+			if (!isSuccessful) {
+				const errMessage = `YorkieSyncPlugin: Failed to sync the document
+				CM: ${this.view.state.doc.toString()}
+				Yorkie: ${this._doc.getRoot().content.toString()}`;
+
+				console.error(errMessage);
+				Sentry.captureMessage(errMessage);
 			}
 		}
 	}
