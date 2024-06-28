261eb325d3e65bbda64ccf640b6dffbce3a37b20
devleejb <devleejb@gmail.com>
2024-06-29 01:00:30 +0900

    Add settings api

--- backend/src/settings/settings.controller.ts
+++ backend/src/settings/settings.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
+import { SettingsService } from "./settings.service";
+import { ApiBearerAuth, ApiFoundResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { GetSettingsResponse } from "./types/get-settings-response.type";
 
+@ApiTags("Settings")
+@ApiBearerAuth()
 @Controller("settings")
-export class SettingsController {}
+export class SettingsController {
+	constructor(private settingsService: SettingsService) {}
+
+	@Get()
+	@ApiOperation({
+		summary: "Get Settings",
+		description: "Get Settings of CodePair",
+	})
+	@ApiFoundResponse({ type: GetSettingsResponse })
+	async getSettings(): Promise<GetSettingsResponse> {
+		return this.settingsService.getSettings();
+	}
+}
--- backend/src/settings/settings.service.ts
+++ backend/src/settings/settings.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { generateFeatureList } from "src/utils/constants/yorkie-intelligence";
+import { GetSettingsResponse } from "./types/get-settings-response.type";
 
 @Injectable()
-export class SettingsService {}
+export class SettingsService {
+	constructor(private configService: ConfigService) {}
+
+	async getSettings(): Promise<GetSettingsResponse> {
+		return {
+			yorkieIntelligence: {
+				enable: this.configService.get("YORKIE_INTELLIGENCE") === "true",
+				config: {
+					features: generateFeatureList(this.configService),
+				},
+			},
+			fileUpload: {
+				enable: this.configService.get("FILE_UPLOAD") === "true",
+			},
+		};
+	}
+}
