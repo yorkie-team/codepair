84ce541f7872bb64fd0406be92951d87a0ab175d
Kevin Park <krapi0314@gmail.com>
2024-07-25 22:36:33 +0900

    Update Dockerfile for arm64 build support on export feature (#251)

--- backend/src/files/files.service.ts
+++ backend/src/files/files.service.ts
@@ -1,6 +1,7 @@
 import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import {
+	BadRequestException,
 	Injectable,
 	NotFoundException,
 	UnauthorizedException,
@@ -8,10 +9,12 @@ import {
 } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { Workspace } from "@prisma/client";
+import * as htmlPdf from "html-pdf-node";
 import * as MarkdownIt from "markdown-it";
 import { PrismaService } from "src/db/prisma.service";
 import { generateRandomKey } from "src/utils/functions/random-string";
 import { CreateUploadPresignedUrlResponse } from "./types/create-upload-url-response.type";
+import { ExportFileRequestBody, ExportFileResponse } from "./types/export-file.type";
 
 @Injectable()
 export class FilesService {
@@ -76,50 +79,50 @@ export class FilesService {
 		}
 	}
 
-	// async exportMarkdown(
-	// 	exportFileRequestBody: ExportFileRequestBody
-	// ): Promise<ExportFileResponse> {
-	// 	const { exportType, content, fileName } = exportFileRequestBody;
+	async exportMarkdown(
+		exportFileRequestBody: ExportFileRequestBody
+	): Promise<ExportFileResponse> {
+		const { exportType, content, fileName } = exportFileRequestBody;
 
-	// 	switch (exportType) {
-	// 		case "markdown":
-	// 			return this.exportToMarkdown(content, fileName);
-	// 		case "html":
-	// 			return this.exportToHtml(content, fileName);
-	// 		case "pdf":
-	// 			return this.exportToPdf(content, fileName);
-	// 		default:
-	// 			throw new BadRequestException("Invalid export type");
-	// 	}
-	// }
+		switch (exportType) {
+			case "markdown":
+				return this.exportToMarkdown(content, fileName);
+			case "html":
+				return this.exportToHtml(content, fileName);
+			case "pdf":
+				return this.exportToPdf(content, fileName);
+			default:
+				throw new BadRequestException("Invalid export type");
+		}
+	}
 
-	// private async exportToMarkdown(content: string, fileName: string): Promise<ExportFileResponse> {
-	// 	return {
-	// 		fileContent: Buffer.from(content),
-	// 		mimeType: "text/markdown",
-	// 		fileName: `${fileName}.md`,
-	// 	};
-	// }
+	private async exportToMarkdown(content: string, fileName: string): Promise<ExportFileResponse> {
+		return {
+			fileContent: Buffer.from(content),
+			mimeType: "text/markdown",
+			fileName: `${fileName}.md`,
+		};
+	}
 
-	// private async exportToHtml(content: string, fileName: string): Promise<ExportFileResponse> {
-	// 	const html = this.markdown.render(content);
-	// 	return {
-	// 		fileContent: Buffer.from(html),
-	// 		mimeType: "text/html",
-	// 		fileName: `${fileName}.html`,
-	// 	};
-	// }
+	private async exportToHtml(content: string, fileName: string): Promise<ExportFileResponse> {
+		const html = this.markdown.render(content);
+		return {
+			fileContent: Buffer.from(html),
+			mimeType: "text/html",
+			fileName: `${fileName}.html`,
+		};
+	}
 
-	// private async exportToPdf(content: string, fileName: string): Promise<ExportFileResponse> {
-	// 	const html = this.markdown.render(content);
-	// 	const options = { format: "A4" };
-	// 	const file = { content: html };
+	private async exportToPdf(content: string, fileName: string): Promise<ExportFileResponse> {
+		const html = this.markdown.render(content);
+		const options = { format: "A4" };
+		const file = { content: html };
 
-	// 	const pdfBuffer = await htmlPdf.generatePdf(file, options);
-	// 	return {
-	// 		fileContent: pdfBuffer,
-	// 		mimeType: "application/pdf",
-	// 		fileName: `${fileName}.pdf`,
-	// 	};
-	// }
+		const pdfBuffer = await htmlPdf.generatePdf(file, options);
+		return {
+			fileContent: pdfBuffer,
+			mimeType: "application/pdf",
+			fileName: `${fileName}.pdf`,
+		};
+	}
 }
