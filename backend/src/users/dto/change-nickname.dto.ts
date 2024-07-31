ec9774fa03f88cb1b6dd2ac5051a8852b45f93aa
mj <minai621@naver.com>
2024-07-31 23:43:22 +0900

    Feat update validation for nickname and title length

--- backend/src/users/dto/change-nickname.dto.ts
+++ backend/src/users/dto/change-nickname.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { MinLength } from "class-validator";
 
 export class ChangeNicknameDto {
-	@ApiProperty({ type: String, description: "Nickname of user to update" })
+	@ApiProperty({ type: String, description: "Nickname of user to update", minLength: 2 })
+	@MinLength(2)
 	nickname: string;
 }
