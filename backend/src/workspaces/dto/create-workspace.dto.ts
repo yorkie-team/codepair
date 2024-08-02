40d73bb992ba542339af78ecab589a029f4ee411
MJ <80272444+minai621@users.noreply.github.com>
2024-08-02 23:15:16 +0900

    Change slug usage to encoded text (#261)
    * Chore remove slugify
    
    * Feat change slug to encoding text
    
    * Feat decoded workspace_slug to encoding back
    
    * Feat Add validate title length in CreateModal
    
    * Refactor remove duplicated error check
    
    * Feat update validation for nickname and title length

--- backend/src/workspaces/dto/create-workspace.dto.ts
+++ backend/src/workspaces/dto/create-workspace.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { MinLength } from "class-validator";
 
 export class CreateWorkspaceDto {
-	@ApiProperty({ description: "Title of project to create", type: String })
+	@ApiProperty({ description: "Title of project to create", type: String, minLength: 2 })
+	@MinLength(2)
 	title: string;
 }
