aadd96ccf727a52aae0e28eea3bb9e1f0d61d452
sihyeong671 <bshlab671@naver.com>
2024-08-15 01:47:26 +0900

    Add ollama llm for yorkie intelligence # 255
    - add docker image in docker compose file
    - change yorkie intelligence env var
    - add lib related to ollama

--- backend/src/langchain/langchain.module.ts
+++ backend/src/langchain/langchain.module.ts
@@ -1,10 +1,23 @@
 import { Module } from "@nestjs/common";
 import { ChatOpenAI } from "@langchain/openai";
+import { ChatOllama } from "@langchain/ollama";
 import { BaseChatModel } from "@langchain/core/language_models/chat_models";
 
 const chatModelFactory = {
 	provide: "ChatModel",
-	useFactory: () => new ChatOpenAI({ modelName: "gpt-4o-mini" }) as BaseChatModel,
+	useFactory: () => {
+		const modelType = process.env.YORKIE_INTELLIGENCE;
+		if (modelType === "gemma2:2b"){
+			return new ChatOllama({
+				model: modelType,
+				checkOrPullModel: true,
+				streaming: true,
+			});
+		}
+		else if (modelType === "openai"){
+			return new ChatOpenAI({ modelName: "gpt-4o-mini" }) as BaseChatModel;
+		}
+	}
 };
 
 @Module({
