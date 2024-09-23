b8627772c920efdef012b2b6e80d79cfad11d298
LeeJongBeom <52884648+devleejb@users.noreply.github.com>
2024-09-23 16:28:50 +0900

    Fix Race Condition in Axios Interceptor for Refresh Token (#356)
    * Fix refresh token race condition
    
    * Change the login loading condition
    
    * Improve variable readability
    
    * Fix typo

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -9,14 +9,15 @@ interface AuthProviderProps {
 
 function AuthProvider(props: AuthProviderProps) {
 	const { children } = props;
-	const { data: user, isSuccess, isLoading } = useGetUserQuery();
+	const { data: user, isSuccess, isLoading, isPending } = useGetUserQuery();
 	const shouldChangeNickname = useMemo(
 		() => isSuccess && !user.nickname,
 		[isSuccess, user?.nickname]
 	);
+	const isAuthLoading = isLoading || isPending;
 
 	return (
-		<AuthContext.Provider value={{ isLoggedIn: isSuccess, isLoading }}>
+		<AuthContext.Provider value={{ isLoggedIn: isSuccess, isLoading: isAuthLoading }}>
 			{shouldChangeNickname ? <ChangeNicknameModal open /> : children}
 		</AuthContext.Provider>
 	);
