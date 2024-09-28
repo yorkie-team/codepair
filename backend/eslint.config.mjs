2136ef7dd860540897e423b1a505c46303edb9b4
choidabom <dabomi0305@naver.com>
2024-09-29 01:58:02 +0900

    Change off to error

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -23,7 +23,7 @@ export default [
 			"@typescript-eslint/interface-name-prefix": "off",
 			"@typescript-eslint/explicit-function-return-type": "off",
 			"@typescript-eslint/explicit-module-boundary-types": "off",
-			"@typescript-eslint/no-explicit-any": "off",
+			"@typescript-eslint/no-explicit-any": "error",
 		},
 	},
 ];
