81244e47d807a70f038808778eff3e8ea69f4904
mj <minai621@naver.com>
2024-10-04 16:33:55 +0900

    feat: integrate MinIO for file storage - Add MinIO support in FilesService - Update docker-compose.yml to include MinIO service - Implement environment variable switching between S3 and MinIO - Add MinIO endpoint configuration - Update documentation for MinIO setup and usage

--- backend/src/files/files.service.ts
+++ backend/src/files/files.service.ts
@@ -1,4 +1,4 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { GetObjectCommand, PutObjectCommand, S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import {
 	BadRequestException,
@@ -14,6 +14,7 @@ import * as htmlPdf from "html-pdf-node";
 import * as MarkdownIt from "markdown-it";
 import { PrismaService } from "src/db/prisma.service";
 import { generateRandomKey } from "src/utils/functions/random-string";
+import { StorageType } from "src/utils/types/storage.type";
 import { CreateUploadPresignedUrlResponse } from "./types/create-upload-url-response.type";
 import { ExportFileRequestBody, ExportFileResponse } from "./types/export-file.type";
 
@@ -125,4 +126,27 @@ export class FilesService {
 			fileName: `${fileName}.pdf`,
 		};
 	}
+
+	private getStorageConfig = (): S3ClientConfig => {
+		const bucketType: StorageType = this.configService.get("BUCKET_TYPE") || "S3";
+		const region = this.configService.get("AWS_REGION") || "us-east-1";
+		if (bucketType === "MINIO") {
+			const endpoint = this.configService.get("MINIO_ENDPOINT");
+			const accessKeyId = this.configService.get("MINIO_ACCESS_KEY");
+			const secretAccessKey = this.configService.get("MINIO_SECRET_KEY");
+			return {
+				region,
+				endpoint,
+				forcePathStyle: true,
+				credentials: {
+					accessKeyId,
+					secretAccessKey,
+				},
+			};
+		}
+
+		return {
+			region,
+		};
+	};
 }
