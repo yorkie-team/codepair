89f0a1d61f7b4fbc5c5c03cbf0467bc1559dafb7
sihyeong671 <bshlab671@naver.com>
2024-10-19 15:32:59 +0900

    refactor code
    - change process to configService.get("YORKIE_INTELLIGENCE")

--- backend/src/langchain/langchain.module.ts
+++ backend/src/langchain/langchain.module.ts
@@ -2,17 +2,17 @@ import { Module } from "@nestjs/common";
 import { ChatOpenAI } from "@langchain/openai";
 import { ChatOllama } from "@langchain/ollama";
 import { BaseChatModel } from "@langchain/core/language_models/chat_models";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 const chatModelFactory = {
 	provide: "ChatModel",
-	useFactory: () => {
-		const modelType = process.env.YORKIE_INTELLIGENCE;
+	useFactory: (configService: ConfigService) => {
+		const modelType = configService.get("YORKIE_INTELLIGENCE");
 		try {
 			// Split the modelType string into provider and model
 			// ex) "ollama:gemma2:2b" => ["ollama", "gemma2:2b"]
 			const [provider, model] = modelType.split(/:(.+)/);
 			let chatModel: BaseChatModel | ChatOllama;
-
 			if (provider === "ollama") {
 				chatModel = new ChatOllama({
 					model: model,
@@ -29,6 +29,7 @@ const chatModelFactory = {
 			throw new Error(`${modelType} is not found. Please check your model name`);
 		}
 	},
+	inject: [ConfigService],
 };
 
 @Module({
