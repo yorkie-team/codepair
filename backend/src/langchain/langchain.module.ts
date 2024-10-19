362a99875461916d6fe84108d9447406f6e0a8ee
sihyeong671 <bshlab671@naver.com>
2024-10-19 14:58:48 +0900

    remove llm model type

--- backend/src/langchain/langchain.module.ts
+++ backend/src/langchain/langchain.module.ts
@@ -3,24 +3,6 @@ import { ChatOpenAI } from "@langchain/openai";
 import { ChatOllama } from "@langchain/ollama";
 import { BaseChatModel } from "@langchain/core/language_models/chat_models";
 
-type ModelList = {
-	[key: string]: string[];
-};
-
-const modelList: ModelList = {
-	ollama: [
-		"lamma3.1",
-		"gemma2",
-		"gemma2:2b",
-		"phi3",
-		"mistral",
-		"neural-chat",
-		"starling-lm",
-		"solar",
-	],
-	openai: ["gpt-3.5-turbo", "gpt-4o-mini"],
-};
-
 const chatModelFactory = {
 	provide: "ChatModel",
 	useFactory: () => {
@@ -31,20 +13,16 @@ const chatModelFactory = {
 			const [provider, model] = modelType.split(/:(.+)/);
 			let chatModel: BaseChatModel | ChatOllama;
 
-			if (modelList[provider]?.includes(model)) {
-				if (provider === "ollama") {
-					chatModel = new ChatOllama({
-						model: model,
-						baseUrl: process.env.OLLAMA_HOST_URL,
-						checkOrPullModel: true,
-						streaming: true,
-					});
-				} else if (provider === "openai") {
-					chatModel = new ChatOpenAI({ modelName: model });
-				}
+			if (provider === "ollama") {
+				chatModel = new ChatOllama({
+					model: model,
+					baseUrl: process.env.OLLAMA_HOST_URL,
+					checkOrPullModel: true,
+					streaming: true,
+				});
+			} else if (provider === "openai") {
+				chatModel = new ChatOpenAI({ modelName: model });
 			}
-
-			if (!chatModel) throw new Error();
 
 			return chatModel;
 		} catch {
