4825fe8a5bc5c6c0014f19fa444105b46caf2215
Changyu Moon <window9u@konkuk.ac.kr>
2025-01-24 20:03:58 +0900

    Add config test structure

---
+++ backend-go/cmd/codepair/config_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestInitConfig(t *testing.T) {
+
+	t.Run("Environment variables only", func(t *testing.T) {
+	})
+
+	t.Run("Configuration file only", func(t *testing.T) {
+	})
+
+	t.Run("Default values (no env, no file)", func(t *testing.T) {
+	})
+
+	t.Run("Environment override config file (placeholder)", func(t *testing.T) {
+	})
+
+	t.Run("Config file override default values (placeholder)", func(t *testing.T) {
+	})
+}
