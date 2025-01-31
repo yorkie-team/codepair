bc35acabf33809113497363671fca2e9e2b50d3e
Changyu Moon <window9u@konkuk.ac.kr>
2025-01-31 13:33:27 +0900

    Add graceful shutdown

--- backend-go/cmd/codepair/main.go
+++ backend-go/cmd/codepair/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/yorkie-team/codepair/backend/internal/config"
 	"github.com/yorkie-team/codepair/backend/internal/server"
@@ -13,6 +17,18 @@ func main() {
 	cp := server.New(conf)
 
 	if err := cp.Start(); err != nil {
-		log.Fatal(err)
+		log.Printf("server error: %v", err)
+		os.Exit(1)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+	<-ctx.Done()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := cp.Shutdown(ctx); err != nil {
+		log.Printf("server shutdown error: %v", err)
 	}
 }
--- backend-go/internal/server/server.go
+++ backend-go/internal/server/server.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -22,7 +24,7 @@ func New(conf *config.Config) *Codepair {
 	s := &http.Server{
 		Addr:        fmt.Sprintf(":%d", conf.Server.Port),
 		Handler:     e,
-		ReadTimeout: conf.Server.ParseReadTimeout(),
+		ReadTimeout: conf.Server.ReadTimeout,
 	}
 
 	cp := &Codepair{
@@ -33,8 +35,22 @@ func New(conf *config.Config) *Codepair {
 }
 
 func (c *Codepair) Start() error {
-	if err := c.httpServer.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
-		return fmt.Errorf("failed to start HTTP server: %w", err)
+	go func() {
+		log.Printf("Server starts running on %d\n", c.config.Server.Port)
+		if err := c.httpServer.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("server error: %v", err)
+		}
+	}()
+	return nil
+}
+
+func (c *Codepair) Shutdown(ctx context.Context) error {
+	log.Println("Gracefully shutting down HTTP server...")
+
+	if err := c.httpServer.Shutdown(ctx); err != nil {
+		return fmt.Errorf("error during http server shutdown: %w", err)
 	}
+
+	log.Println("Server successfully shut down")
 	return nil
 }
