133232ede03ee43541909ef9c9ff46c44d700c24
Changyu Moon <window9u@konkuk.ac.kr>
2025-02-01 11:34:57 +0900

    Remove graceful shutdown
    - this is not needed for now
    - this implementation makes more complexity for contribution

--- backend-go/cmd/codepair/main.go
+++ backend-go/cmd/codepair/main.go
@@ -1,41 +1,18 @@
 package main
 
 import (
-	"context"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/yorkie-team/codepair/backend/internal/config"
 	"github.com/yorkie-team/codepair/backend/internal/server"
 )
 
-const gracefulWaitingTime = 30 * time.Second
-
 func main() {
 	conf := config.LoadConfig()
 	cp := server.New(conf)
 
-	go func() {
-		if err := cp.Start(); err != nil {
-			log.Fatalf("Failed to start server: %v", err)
-		}
-	}()
-
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
-	<-ctx.Done()
-	log.Printf("Server will shut down in %v", gracefulWaitingTime)
-
-	ctx, cancel := context.WithTimeout(context.Background(), gracefulWaitingTime)
-	defer cancel()
-
-	if err := cp.Shutdown(ctx); err != nil {
-		log.Fatalf("Server shutdown failed: %v", err)
+	if err := cp.Start(); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
 	}
 
-	log.Println("Server exited gracefully.")
 }
--- backend-go/internal/server/server.go
+++ backend-go/internal/server/server.go
@@ -1,44 +1,36 @@
 package server
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+
 	"github.com/yorkie-team/codepair/backend/internal/config"
 )
 
-type Codepair struct {
+type CodePair struct {
 	config *config.Config
 	echo   *echo.Echo
 }
 
-func New(conf *config.Config) *Codepair {
+func New(conf *config.Config) *CodePair {
 	e := echo.New()
 
 	RegisterRoutes(e)
 
-	cp := &Codepair{
+	cp := &CodePair{
 		config: conf,
 		echo:   e,
 	}
 	return cp
 }
 
-func (c *Codepair) Start() error {
+func (c *CodePair) Start() error {
 	addr := fmt.Sprintf(":%d", c.config.Server.Port)
 	if err := c.echo.Start(addr); !errors.Is(err, http.ErrServerClosed) {
 		return fmt.Errorf("failed to start server: %w", err)
 	}
-	return nil
-}
-
-func (c *Codepair) Shutdown(ctx context.Context) error {
-	if err := c.echo.Shutdown(ctx); err != nil {
-		return fmt.Errorf("shutdown failed: %w", err)
-	}
-
 	return nil
 }
