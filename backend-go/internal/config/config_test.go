244af84d886c1ce239e16009da47a2a7b04f4e34
Changyu Moon <window9u@konkuk.ac.kr>
2025-02-02 18:17:35 +0900

    add empty test

---
+++ backend-go/internal/config/config_test.go
@@ -0,0 +1,21 @@
+package config
+
+import "testing"
+
+func TestInitConfig(t *testing.T) {
+
+	t.Run("Config with only environment variables test", func(t *testing.T) {
+	})
+
+	t.Run("Config with only yaml file test", func(t *testing.T) {
+	})
+
+	t.Run("Config with only Default values test", func(t *testing.T) {
+	})
+
+	t.Run("Environment override config file test", func(t *testing.T) {
+	})
+
+	t.Run("Config file override default values test", func(t *testing.T) {
+	})
+}
