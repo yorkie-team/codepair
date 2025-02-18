445fcf2b37b56682fdf2b16cb7863d0cba1a7e1b
Changyu Moon <window9u@konkuk.ac.kr>
2025-02-18 14:58:23 +0900

    Implement User repository

--- backend-go/internal/core/user/model.go
+++ backend-go/internal/core/user/model.go
@@ -1,1 +1,14 @@
 package user
+
+import (
+	"time"
+)
+
+type User struct {
+	ID             string
+	SocialProvider string
+	SocialUID      string
+	Nickname       *string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
