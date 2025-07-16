d6a4537b9c20888da290a23e5d4fad526a10f5e5
Seungyong Lee <kokodakadokok@gmail.com>
2025-07-16 23:38:23 +0900

    Align with updated GetDocuments API request/response format (#497)
    * Align with Yorkie #1391 feature
    
    * Change type more specific

--- backend/src/workspace-documents/types/find-documents-from-yorkie-response.type.ts
+++ backend/src/workspace-documents/types/find-documents-from-yorkie-response.type.ts
@@ -1,7 +1,8 @@
 class YorkieDocument {
 	id: string;
 	key: string;
-	snapshot: string;
+	root?: string;
+	presences?: { [clientId: string]: { data: { [key: string]: string } } };
 	createdAt: string;
 	updatedAt: string;
 	accessedAt: string;
