863960fee49e324db84d09db6e4434be3645ece2
Kim Ji Yu <96280450+Banal972@users.noreply.github.com>
2025-08-04 12:15:23 +0900

    desktop Prettier write

--- desktop/out/main/index.js
+++ desktop/out/main/index.js
@@ -5,46 +5,46 @@ const __filename = import.meta.filename;
 const __dirname = import.meta.dirname;
 const require2 = __cjs_mod__.createRequire(import.meta.url);
 const is = {
-  dev: !app.isPackaged
+	dev: !app.isPackaged,
 };
 ({
-  isWindows: process.platform === "win32",
-  isMacOS: process.platform === "darwin",
-  isLinux: process.platform === "linux"
+	isWindows: process.platform === "win32",
+	isMacOS: process.platform === "darwin",
+	isLinux: process.platform === "linux",
 });
 const createWindow = async () => {
-  try {
-    const win = new BrowserWindow({
-      show: false,
-      maximizable: true,
-      webPreferences: {
-        preload: join(__dirname, "../preload/index.js"),
-        sandbox: false
-      }
-    });
-    win.maximize();
-    win.show();
-    const serviceUrl = is.dev ? "http://localhost:5173" : "https://codepair.yorkie.dev";
-    await win.loadURL(serviceUrl);
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error("Error creating the browser window:", error.message);
-      dialog.showErrorBox(
-        "Application Error",
-        `Failed to start application: ${error.message}
+	try {
+		const win = new BrowserWindow({
+			show: false,
+			maximizable: true,
+			webPreferences: {
+				preload: join(__dirname, "../preload/index.js"),
+				sandbox: false,
+			},
+		});
+		win.maximize();
+		win.show();
+		const serviceUrl = is.dev ? "http://localhost:5173" : "https://codepair.yorkie.dev";
+		await win.loadURL(serviceUrl);
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error("Error creating the browser window:", error.message);
+			dialog.showErrorBox(
+				"Application Error",
+				`Failed to start application: ${error.message}
 
 The application will now quit.`
-      );
-      app.quit();
-    }
-  }
+			);
+			app.quit();
+		}
+	}
 };
 app.whenReady().then(createWindow);
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
-app.on("activate", function() {
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+app.on("activate", function () {
+	if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
