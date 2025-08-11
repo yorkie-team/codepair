d2b136a355bc9d4711406d343a574a39f1718681
ezcolin2 <growth_s@naver.com>
2025-08-11 10:30:13 +0900

    feat add max length option (120) to create-workspace.dto and update-workspace.dto

--- backend/src/workspaces/dto/create-workspace.dto.ts
+++ backend/src/workspaces/dto/create-workspace.dto.ts
@@ -2,7 +2,12 @@ import { ApiProperty } from "@nestjs/swagger";
 import { MinLength } from "class-validator";
 
 export class CreateWorkspaceDto {
-	@ApiProperty({ description: "Title of project to create", type: String, minLength: 2 })
+	@ApiProperty({
+		description: "Title of project to create",
+		type: String,
+		minLength: 2,
+		maxLength: 120,
+	})
 	@MinLength(2)
 	title: string;
 }
--- backend/src/workspaces/dto/update-workspace-title.dto.ts
+++ backend/src/workspaces/dto/update-workspace-title.dto.ts
@@ -2,7 +2,12 @@ import { ApiProperty } from "@nestjs/swagger";
 import { MinLength } from "class-validator";
 
 export class UpdateWorkspaceTitleDto {
-	@ApiProperty({ description: "New title of the workspace", type: String, minLength: 2 })
+	@ApiProperty({
+		description: "New title of the workspace",
+		type: String,
+		minLength: 2,
+		maxLength: 120,
+	})
 	@MinLength(2)
 	title: string;
 }
